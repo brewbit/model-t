3723af1cf8969454b47d864985f1d69c7ad16d90
nick <nick@ad39a8c3-5e57-4819-ba6d-ac3c945a73f6>
2013-05-28 07:04:04 +0000

    Reduce ADC resolution. Cleanup touch driver.
    git-svn-id: http://svn.inebriatedinnovations.com/repo/BrewBit/sw/b2tc-ChibiOS@499 ad39a8c3-5e57-4819-ba6d-ac3c945a73f6

--- src/touch.c
+++ src/touch.c
@@ -7,9 +7,12 @@
 
 #include <stdbool.h>
 
-void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n);
-void setup_y_axis(void);
-void setup_x_axis(void);
+static void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n);
+static void setup_axis(u16 rp, u16 rn, u16 bp, u16 bn);
+static void setup_y_axis(void);
+static void setup_x_axis(void);
+static adcsample_t adc_avg(adcsample_t* samples, uint16_t num_samples);
+static msg_t touch_thread(void* arg);
 
 
 // X+ = PA4 = ADC1-4
@@ -46,7 +49,7 @@ static const ADCConversionGroup adcgrpcfg = {
   adccb,
   NULL,
   /* HW dependent part.*/
-  0,
+  ADC_CR1_RES_0,
   ADC_CR2_SWSTART,
   0,
   ADC_SMPR2_SMP_AN4(ADC_SAMPLE_480) | ADC_SMPR2_SMP_AN5(ADC_SAMPLE_480),
@@ -57,8 +60,11 @@ static const ADCConversionGroup adcgrpcfg = {
 
 static adcsample_t avg_y, avg_x;
 static volatile uint8_t reading_ready;
+static uint8_t wa_touch_thread[1024];
 
-void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
+
+static void
+adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
 
   (void) buffer; (void) n;
   /* Note, only in the ADC_COMPLETE state because the ADC driver fires an
@@ -71,38 +77,29 @@ void adccb(ADCDriver *adcp, adcsample_t *buffer, size_t n) {
     avg_y = (samples[0] + samples[2] + samples[4] + samples[6]) / 4;
     avg_x = (samples[1] + samples[3] + samples[5] + samples[7]) / 4;
 
-    /* Starts an asynchronous ADC conversion operation, the conversion
-       will be executed in parallel to the current PWM cycle and will
-       terminate before the next PWM cycle.*/
-    setup_y_axis();
-
     reading_ready = 1;
   }
 }
 
-uint8_t wa_touch_thread[1024];
-msg_t
-touch_thread(void* arg);
-
 void
 touch_init()
 {
-  terminal_write("in touch init\n");
   chThdCreateStatic(wa_touch_thread, sizeof(wa_touch_thread), NORMALPRIO, touch_thread, NULL);
 
   adcStart(&ADCD1, NULL);
-  adcSTM32EnableTSVREFE();
 
 
   /* Starts an asynchronous ADC conversion operation, the conversion
      will be executed in parallel to the current PWM cycle and will
      terminate before the next PWM cycle.*/
   chSysLockFromIsr();
+  setup_y_axis();
   adcStartConversionI(&ADCD1, &adcgrpcfg, samples, ADC_GRP1_BUF_DEPTH);
   chSysUnlockFromIsr();
 }
 
-void setup_axis(u16 rp, u16 rn, u16 bp, u16 bn)
+static void
+setup_axis(u16 rp, u16 rn, u16 bp, u16 bn)
 {
   palSetPadMode(GPIOA, rp, PAL_MODE_INPUT_ANALOG);
   palSetPadMode(GPIOA, rn, PAL_MODE_INPUT);
@@ -113,24 +110,23 @@ void setup_axis(u16 rp, u16 rn, u16 bp, u16 bn)
   palClearPad(GPIOA, bn);
 }
 
-void setup_y_axis()
+static void
+setup_y_axis()
 {
   setup_axis(YP, YN, XP, XN);
 }
 
-void setup_x_axis()
+static void
+setup_x_axis()
 {
   setup_axis(XP, XN, YP, YN);
 }
 
-
-
-msg_t
+static msg_t
 touch_thread(void* arg)
 {
   (void)arg;
 
-  terminal_write("in touch thread\n");
   while (1) {
     if (reading_ready) {
       reading_ready = 0;
@@ -139,15 +135,13 @@ touch_thread(void* arg)
       int i,j;
       for (i = 0; i < ADC_GRP1_NUM_CHANNELS; ++i) {
         terminal_write("channel:\n");
-        int avg = 0;
         for (j = 0; j < ADC_GRP1_BUF_DEPTH; ++j) {
           terminal_write_int(samples[i*ADC_GRP1_BUF_DEPTH + j]);
           terminal_write(" ");
-          avg += samples[i*ADC_GRP1_BUF_DEPTH + j];
         }
-        avg /= ADC_GRP1_BUF_DEPTH;
+        adcsample_t avg_val = adc_avg(samples + i*ADC_GRP1_BUF_DEPTH + 2, ADC_GRP1_BUF_DEPTH-2);
         terminal_write("avg: ");
-        terminal_write_int(avg);
+        terminal_write_int(avg_val);
         terminal_write("\n");
       }
 
@@ -155,6 +149,7 @@ touch_thread(void* arg)
       chThdSleepMilliseconds(500);
 
       chSysLockFromIsr();
+      setup_y_axis();
       adcStartConversionI(&ADCD1, &adcgrpcfg, samples, ADC_GRP1_BUF_DEPTH);
       chSysUnlockFromIsr();
     }
@@ -163,3 +158,16 @@ touch_thread(void* arg)
   return 0;
 }
 
+adcsample_t
+adc_avg(adcsample_t* samples, uint16_t num_samples)
+{
+  uint32_t avg_sample = 0;
+  int i;
+
+  for (i = 0; i < num_samples; ++i) {
+    avg_sample += samples[i];
+  }
+  avg_sample /= num_samples;
+
+  return avg_sample;
+}
