3be16049d666fd34af36d56959ece1580a9d06e7
nick <nick@ad39a8c3-5e57-4819-ba6d-ac3c945a73f6>
2013-05-31 06:49:25 +0000

    Calculate touch pressure based on TI App Note SBAA155A. Use touch pressure to determine if the screen has actually been touched instead of the raw coordinates.
    git-svn-id: http://svn.inebriatedinnovations.com/repo/BrewBit/sw/b2tc-ChibiOS@513 ad39a8c3-5e57-4819-ba6d-ac3c945a73f6

--- src/touch.c
+++ src/touch.c
@@ -10,9 +10,9 @@
 #include <stdbool.h>
 
 
-#define XP 5
+#define XP 4
 #define XN 6
-#define YP 4
+#define YP 5
 #define YN 7
 
 #define NUM_SAMPLES 8
@@ -21,13 +21,16 @@
 #define TOUCH_THRESHOLD 40
 #define DEBOUNCE_TIME MS2ST(100)
 
+// ADC sample resolution
+#define Q 1024
+
 #define MAX_TOUCH_VELOCITY 10
 
 typedef struct {
-  uint16_t read_pos_pad;
-  uint16_t read_neg_pad;
-  uint16_t pull_pos_pad;
-  uint16_t pull_neg_pad;
+  uint16_t drive_pos_pad;
+  uint16_t drive_neg_pad;
+  uint16_t sample_pos_pad;
+  uint16_t sample_neg_pad;
   const ADCConversionGroup* conv_grp;
 } axis_cfg_t;
 
@@ -37,7 +40,7 @@ static adcsample_t adc_avg(adcsample_t* samples, uint16_t num_samples);
 static msg_t touch_thread(void* arg);
 
 
-static const ADCConversionGroup y_conv_grp = {
+static const ADCConversionGroup xp_conv_grp = {
   .circular = FALSE,
   .num_channels = 1,
   .end_cb = NULL,
@@ -53,7 +56,7 @@ static const ADCConversionGroup y_conv_grp = {
   .sqr3  = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN4),
 };
 
-static const ADCConversionGroup x_conv_grp = {
+static const ADCConversionGroup yp_conv_grp = {
   .circular = FALSE,
   .num_channels = 1,
   .end_cb = NULL,
@@ -69,42 +72,59 @@ static const ADCConversionGroup x_conv_grp = {
   .sqr3  = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN5),
 };
 
+static const ADCConversionGroup yn_conv_grp = {
+  .circular = FALSE,
+  .num_channels = 1,
+  .end_cb = NULL,
+  .error_cb = NULL,
+
+  /* HW dependent part.*/
+  .cr1   = ADC_CR1_RES_0, // 10-bit resolution
+  .cr2   = ADC_CR2_SWSTART, // software triggered
+  .smpr1 = 0,
+  .smpr2 = ADC_SMPR2_SMP_AN7(ADC_SAMPLE_480),
+  .sqr1  = ADC_SQR1_NUM_CH(1),
+  .sqr2  = 0,
+  .sqr3  = ADC_SQR3_SQ1_N(ADC_CHANNEL_IN7),
+};
+
+static const axis_cfg_t z1_axis = {
+    .drive_pos_pad = YP,
+    .drive_neg_pad = XN,
+    .sample_pos_pad = XP,
+    .sample_neg_pad = YN,
+    .conv_grp = &xp_conv_grp,
+};
+
+static const axis_cfg_t z2_axis = {
+    .drive_pos_pad = YP,
+    .drive_neg_pad = XN,
+    .sample_pos_pad = XP,
+    .sample_neg_pad = YN,
+    .conv_grp = &yn_conv_grp,
+};
+
 static const axis_cfg_t x_axis = {
-    .read_pos_pad = XP,
-    .read_neg_pad = XN,
-    .pull_pos_pad = YP,
-    .pull_neg_pad = YN,
-    .conv_grp = &x_conv_grp,
+    .drive_pos_pad = XP,
+    .drive_neg_pad = XN,
+    .sample_pos_pad = YP,
+    .sample_neg_pad = YN,
+    .conv_grp = &yp_conv_grp,
 };
 
 static const axis_cfg_t y_axis = {
-    .read_pos_pad = YP,
-    .read_neg_pad = YN,
-    .pull_pos_pad = XP,
-    .pull_neg_pad = XN,
-    .conv_grp = &y_conv_grp,
+    .drive_pos_pad = YP,
+    .drive_neg_pad = YN,
+    .sample_pos_pad = XP,
+    .sample_neg_pad = XN,
+    .conv_grp = &xp_conv_grp,
 };
 
 static uint8_t wa_touch_thread[1024];
-static matrix_t calib_matrix;
-static point_t raw;
-static point_t calib;
-static uint8_t touch_down;
-static systime_t last_touch_time;
 
 void
 touch_init()
 {
-  point_t perfectScreenSample[3] = {
-    { 100, 100 },
-    { 900, 500 },
-    { 500, 900 }
-  };
-  setCalibrationMatrix(
-      perfectScreenSample,
-      perfectScreenSample,
-      &calib_matrix);
-
   adcStart(&ADCD1, NULL);
 
   chThdCreateStatic(wa_touch_thread, sizeof(wa_touch_thread), NORMALPRIO, touch_thread, NULL);
@@ -116,14 +136,14 @@ read_axis(const axis_cfg_t* axis_cfg)
   adcsample_t samples[NUM_SAMPLES];
 
   /* setup the pad modes */
-  palSetPadMode(GPIOA, axis_cfg->read_pos_pad, PAL_MODE_INPUT_ANALOG);
-  palSetPadMode(GPIOA, axis_cfg->read_neg_pad, PAL_MODE_INPUT_ANALOG);
-  palSetPadMode(GPIOA, axis_cfg->pull_pos_pad, PAL_MODE_OUTPUT_PUSHPULL);
-  palSetPadMode(GPIOA, axis_cfg->pull_neg_pad, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA, axis_cfg->sample_pos_pad, PAL_MODE_INPUT_ANALOG);
+  palSetPadMode(GPIOA, axis_cfg->sample_neg_pad, PAL_MODE_INPUT_ANALOG);
+  palSetPadMode(GPIOA, axis_cfg->drive_pos_pad, PAL_MODE_OUTPUT_PUSHPULL);
+  palSetPadMode(GPIOA, axis_cfg->drive_neg_pad, PAL_MODE_OUTPUT_PUSHPULL);
 
-  /* set the 'pull' pins to the correct state */
-  palSetPad(GPIOA, axis_cfg->pull_pos_pad);
-  palClearPad(GPIOA, axis_cfg->pull_neg_pad);
+  /* set the 'drive' pins to the correct state */
+  palSetPad(GPIOA, axis_cfg->drive_pos_pad);
+  palClearPad(GPIOA, axis_cfg->drive_neg_pad);
 
   /* Let the pins settle */
   chThdSleepMilliseconds(20);
@@ -142,40 +162,38 @@ touch_thread(void* arg)
   (void)arg;
 
   while (1) {
-    point_t new_raw;
-    new_raw.x = read_axis(&x_axis);
-    new_raw.y = read_axis(&y_axis);
+    uint32_t z1 = read_axis(&z1_axis);
+    uint32_t z2 = read_axis(&z2_axis);
+    uint32_t x = read_axis(&x_axis);
+    uint32_t y = read_axis(&y_axis);
 
-    if ((new_raw.x > TOUCH_THRESHOLD) &&
-        (new_raw.y > TOUCH_THRESHOLD)) {
+    /* Calculate pressure of touch based on equations from TI Application Note SBAA155A */
+    /* Prevent divide by zero */
+    z1 = MAX(1, z1);
+    /* Modified form of equation 8 with rx = 1 */
+    uint32_t rz = (((x * z2) / z1) - x) / Q;
+    /* Modified form of equation 9 with a = 1024, b = 1 */
+    uint32_t p = Q - rz;
 
-//      if (touch_down) {
-//        raw.x = (new_raw.x > raw.x) ?
-//            MIN(raw.x + MAX_TOUCH_VELOCITY, new_raw.x) :
-//            MAX(raw.x - MAX_TOUCH_VELOCITY, new_raw.x);
-//        raw.y = (new_raw.y > raw.y) ?
-//            MIN(raw.y + MAX_TOUCH_VELOCITY, new_raw.y) :
-//            MAX(raw.y - MAX_TOUCH_VELOCITY, new_raw.y);
-//      }
-//      else {
-        raw = new_raw;
-//      }
+    terminal_clear();
+    terminal_write("z: ");
+    terminal_write_int(z1);
+    terminal_write(", ");
+    terminal_write_int(z2);
+    terminal_write("\nrz: ");
+    terminal_write_int(rz);
+    terminal_write("\np: ");
+    terminal_write_int(p);
 
-      getDisplayPoint(&calib, &raw, &calib_matrix);
-
-      gui_touch_down(&calib, &raw);
-      touch_down = 1;
-      last_touch_time = chTimeNow();
-    }
-    else {
-      if (touch_down &&
-          !chTimeIsWithin(last_touch_time, last_touch_time + DEBOUNCE_TIME)) {
-        gui_touch_up(&calib, &raw);
-        touch_down = 0;
-      }
+    if (p > 900) {
+      terminal_write("\npos: (");
+      terminal_write_int(x);
+      terminal_write(", ");
+      terminal_write_int(y);
+      terminal_write(")");
     }
 
-    chThdSleepMilliseconds(1);
+    chThdSleepMilliseconds(100);
   }
 
   return 0;
