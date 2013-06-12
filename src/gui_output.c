31e1da4265db563758374f346bcc2ccfe5eff1c9
nick <nick@ad39a8c3-5e57-4819-ba6d-ac3c945a73f6>
2013-06-12 06:10:10 +0000

    Implement all text on the output setup screen with label widgets. Remove output setup screen paint method.
    git-svn-id: http://svn.inebriatedinnovations.com/repo/BrewBit/sw/b2tc-ChibiOS@560 ad39a8c3-5e57-4819-ba6d-ac3c945a73f6

--- src/gui_output.c
+++ src/gui_output.c
@@ -17,15 +17,14 @@ typedef struct {
 } output_screen_t;
 
 
-static void output_settings_screen_paint(paint_event_t* event);
 static void output_settings_screen_destroy(widget_t* w);
-
 static void back_clicked(click_event_t* event);
 
+
 widget_class_t output_settings_widget_class = {
-    .on_paint   = output_settings_screen_paint,
     .on_destroy = output_settings_screen_destroy
 };
+
 
 widget_t*
 output_settings_screen_create()
@@ -50,9 +49,13 @@ output_settings_screen_create()
   s->trigger_button = button_create(s->widget, rect, NULL, img_temp_38, PURPLE, NULL);
 
   rect.x = 85;
-  rect.y = 95;
+  rect.y = 20;
   rect.width = 220;
   rect.height = -1;
+  s->function_header_label = label_create(s->widget, rect, "Output 1 Setup", font_opensans_22, WHITE);
+
+  rect.x = 85;
+  rect.y = 95;
   s->function_header_label = label_create(s->widget, rect, "Function: Heating", font_opensans_16, WHITE);
 
   rect.y = 120;
@@ -83,25 +86,9 @@ output_settings_screen_destroy(widget_t* w)
 
 
 static void
-output_settings_screen_paint(paint_event_t* event)
+back_clicked(click_event_t* event)
 {
   (void)event;
 
-  gfx_set_fg_color(WHITE);
-  gfx_set_font(font_opensans_22);
-  gfx_draw_str("Output 1 Setup", -1, 85, 20);
-
-//  gfx_set_font(font_opensans_16);
-//  gfx_draw_str("Trigger: Probe 2", -1, 85, 165);
-//
-//  gfx_set_font(font_opensans_8);
-//  gfx_draw_str("The output will turn on when the temp", -1, 85, 190);
-//  gfx_draw_str("is below the setpoint.", -1, 85, 205);
-}
-
-
-static void
-back_clicked(click_event_t* event)
-{
   gui_pop_screen();
 }
