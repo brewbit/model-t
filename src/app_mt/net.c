edcb4283185c2eda2e68d0b7e6781fdd17da8219
nick <nick@ad39a8c3-5e57-4819-ba6d-ac3c945a73f6>
2013-09-18 09:36:58 +0000

    Cleanup net driver.
    git-svn-id: http://svn.inebriatedinnovations.com/repo/BrewBit/sw/b2tc-ChibiOS@823 ad39a8c3-5e57-4819-ba6d-ac3c945a73f6

--- src/app_mt/net.c
+++ src/app_mt/net.c
@@ -31,6 +31,9 @@ wlan_read_interupt_pin(void);
 static void
 write_wlan_pin(unsigned char val);
 
+static void
+ota_update_mgr(void);
+
 
 static int connected;
 
@@ -130,60 +133,63 @@ static msg_t
 wlan_thread(void* arg)
 {
   (void)arg;
+
   chRegSetThreadName("wlan");
 
-  chprintf(SD_STDIO, "starting...\r\n");
   wlan_start(0);
 
-  chprintf(SD_STDIO, "getting MAC... ");
-  unsigned char mac[6];
-  nvmem_get_mac_address(mac);
-  chprintf(SD_STDIO, "%02x:%02x:%02x:%02x:%02x:%02x\r\n", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-  chprintf(SD_STDIO, "connecting... ");
   long ret = wlan_connect(WLAN_SEC_WPA2, "internets", 9, NULL, (unsigned char*)"stenretni", 9);
-  chprintf(SD_STDIO, "%d\r\n", ret);
+  if (ret != 0) {
+    chprintf(SD_STDIO, "Could not connect to network\r\n");
+  }
 
-  chprintf(SD_STDIO, "waiting for DHCP... ");
   while (!connected)
     chThdSleepMilliseconds(100);
-  chprintf(SD_STDIO, "OK\r\n");
 
+  ota_update_mgr();
 
+  return 0;
+}
 
+// TODO: move this elsewhere
+static void
+ota_update_mgr()
+{
+  int ret;
   sockaddr_in serv_addr;
 
-
-  chprintf(SD_STDIO, "creating socket... ");
   int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-  chprintf(SD_STDIO, "%d\r\n", s);
+  if (s == -1) {
+    chprintf(SD_STDIO, "FATAL: Could not open socket for OTA update process\r\n");
+    return;
+  }
 
-  memset(&serv_addr, '0', sizeof(serv_addr));
+  memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = 0; // INADDR_ANY
   serv_addr.sin_port = htons(5000);
 
-  chprintf(SD_STDIO, "binding... ");
   ret = bind(s, (sockaddr*)&serv_addr, sizeof(serv_addr));
-  chprintf(SD_STDIO, "%d\r\n", ret);
+  if (ret != 0) {
+    chprintf(SD_STDIO, "FATAL: Could not bind socket for OTA update process\r\n");
+    return;
+  }
 
-  chprintf(SD_STDIO, "listening... ");
   ret = listen(s, 10);
-  chprintf(SD_STDIO, "%d\r\n", ret);
+  if (ret != 0) {
+    chprintf(SD_STDIO, "FATAL: Could not listen on socket for OTA update process\r\n");
+    return;
+  }
 
   while(1) {
-    chprintf(SD_STDIO, "accepting... ");
     sockaddr conn_addr;
     socklen_t conn_addr_len;
     int connfd = accept(s, &conn_addr, &conn_addr_len);
-    chprintf(SD_STDIO, "%d\r\n", connfd);
 
     if (connfd > 0) {
       while (1) {
         char data[16];
-        chprintf(SD_STDIO, "receiving... ");
         ret = recv(connfd, data, sizeof(data)-1, 0);
-        chprintf(SD_STDIO, "%d\r\n", ret);
 
         if (ret > 0) {
           data[ret] = 0;
@@ -194,69 +200,4 @@ wlan_thread(void* arg)
 
     chThdSleepSeconds(5);
   }
-//  unsigned long aiIntervalList[16] = {
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000,
-//      2000
-//  };
-//  chprintf(SD_STDIO, "scanning... ");
-//  ret = wlan_ioctl_set_scan_params(1,
-//      100,
-//      100,
-//      5,
-//      0x1fff,
-//      -110,
-//      0,
-//      205,
-//      aiIntervalList);
-//  chprintf(SD_STDIO, "%d\r\n", ret);
-//
-//  while (1) {
-//    unsigned char res[64];
-//    chprintf(SD_STDIO, "result... ");
-//    wlan_ioctl_get_scan_results(0, res);
-//    unsigned long count = STREAM_TO_UINT32_f(res, 0);
-//    unsigned long status = STREAM_TO_UINT32_f(res, 4);
-//    uint8_t results_valid = res[8] & 0x01;
-//    uint8_t rssi = res[8] >> 1;
-//    uint8_t security = res[9] & 0x03;
-//    uint8_t ssid_len = res[9] >> 2;
-//    uint16_t timestamp = STREAM_TO_UINT16_f(res, 10);
-//
-//    char ssid[33];
-//    memcpy(ssid, &res[12], 32);
-//    ssid[32] = 0;
-//
-//    char bssid[7];
-//    memcpy(bssid, &res[44], 6);
-//    bssid[6] = 0;
-//
-//
-//    chprintf(SD_STDIO, "scan status: %d\r\n", status);
-//    chprintf(SD_STDIO, "  results left: %d\r\n", count);
-//    chprintf(SD_STDIO, "  valid: %d\r\n", results_valid);
-//    chprintf(SD_STDIO, "  rssi: %d\r\n", rssi);
-//    chprintf(SD_STDIO, "  security: %d\r\n", security);
-//    chprintf(SD_STDIO, "  ssid_len: %d\r\n", ssid_len);
-//    chprintf(SD_STDIO, "  timestamp: %d\r\n", timestamp);
-//    chprintf(SD_STDIO, "  ssid: %s\r\n", ssid);
-//    chprintf(SD_STDIO, "  bssid: %x:%x:%x:%x:%x:%x\r\n", bssid[0], bssid[1], bssid[2], bssid[3], bssid[4], bssid[5]);
-//
-//    chThdSleepSeconds(1);
-//  }
-
-  return 0;
 }
