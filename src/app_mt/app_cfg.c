27a37c3b39dceaaaac89102dfd11c1159949c5b4
nick <nick@ad39a8c3-5e57-4819-ba6d-ac3c945a73f6>
2013-10-27 20:14:03 +0000

    Add CRC to app_cfg to ensure that invalid (erased flash) data is not used.
    git-svn-id: http://svn.inebriatedinnovations.com/repo/BrewBit/sw/b2tc-ChibiOS@837 ad39a8c3-5e57-4819-ba6d-ac3c945a73f6

--- src/app_mt/app_cfg.c
+++ src/app_mt/app_cfg.c
@@ -4,26 +4,30 @@
 #include "message.h"
 #include "iflash.h"
 #include "common.h"
+#include "crc/crc32.h"
 
 #include <string.h>
 
 
 typedef struct {
-  bool is_valid;
-
   unit_t temp_unit;
   matrix_t touch_calib;
   sensor_settings_t sensor_settings[NUM_SENSORS];
   output_settings_t output_settings[NUM_OUTPUTS];
-} app_cfg_t;
+} app_cfg_data_t;
+
+typedef struct {
+  app_cfg_data_t data;
+  uint32_t crc;
+} app_cfg_rec_t;
 
 
 /* app_cfg stored in flash */
 __attribute__ ((section("app_cfg")))
-app_cfg_t app_cfg_stored;
+app_cfg_rec_t app_cfg_stored;
 
 /* Local RAM copy of app_cfg */
-static app_cfg_t app_cfg_local;
+static app_cfg_rec_t app_cfg_local;
 static Mutex app_cfg_mtx;
 static systime_t last_idle;
 
@@ -31,40 +35,41 @@ static systime_t last_idle;
 void
 app_cfg_init()
 {
-  if (app_cfg_stored.is_valid) {
+  uint32_t calc_crc = crc32_block(0, &app_cfg_stored.data, sizeof(app_cfg_data_t));
+
+  if (app_cfg_stored.crc == calc_crc) {
     app_cfg_local = app_cfg_stored;
   }
   else {
-    app_cfg_local.temp_unit = UNIT_TEMP_DEG_F;
+    app_cfg_local.data.temp_unit = UNIT_TEMP_DEG_F;
 
-    app_cfg_local.touch_calib.An      = 76320;
-    app_cfg_local.touch_calib.Bn      = 3080;
-    app_cfg_local.touch_calib.Cn      = -9475080;
-    app_cfg_local.touch_calib.Dn      = -560;
-    app_cfg_local.touch_calib.En      = 60340;
-    app_cfg_local.touch_calib.Fn      = -4360660;
-    app_cfg_local.touch_calib.Divider = 205664;
+    app_cfg_local.data.touch_calib.An      = 76320;
+    app_cfg_local.data.touch_calib.Bn      = 3080;
+    app_cfg_local.data.touch_calib.Cn      = -9475080;
+    app_cfg_local.data.touch_calib.Dn      = -560;
+    app_cfg_local.data.touch_calib.En      = 60340;
+    app_cfg_local.data.touch_calib.Fn      = -4360660;
+    app_cfg_local.data.touch_calib.Divider = 205664;
 
-    app_cfg_local.sensor_settings[SENSOR_1].setpoint.unit = UNIT_TEMP_DEG_F;
-    app_cfg_local.sensor_settings[SENSOR_1].setpoint.value = 78;
+    app_cfg_local.data.sensor_settings[SENSOR_1].setpoint.unit = UNIT_TEMP_DEG_F;
+    app_cfg_local.data.sensor_settings[SENSOR_1].setpoint.value = 78;
 
-    app_cfg_local.sensor_settings[SENSOR_2].setpoint.unit = UNIT_TEMP_DEG_F;
-    app_cfg_local.sensor_settings[SENSOR_2].setpoint.value = 78;
+    app_cfg_local.data.sensor_settings[SENSOR_2].setpoint.unit = UNIT_TEMP_DEG_F;
+    app_cfg_local.data.sensor_settings[SENSOR_2].setpoint.value = 78;
 
-    app_cfg_local.output_settings[OUTPUT_1].function = OUTPUT_FUNC_COOLING;
-    app_cfg_local.output_settings[OUTPUT_1].trigger = SENSOR_1;
-    app_cfg_local.output_settings[OUTPUT_1].compressor_delay = S2ST(3* 60);
-    app_cfg_local.output_settings[OUTPUT_1].setpoint.unit = UNIT_TIME_MIN;
-    app_cfg_local.output_settings[OUTPUT_1].setpoint.value = 3;
+    app_cfg_local.data.output_settings[OUTPUT_1].function = OUTPUT_FUNC_COOLING;
+    app_cfg_local.data.output_settings[OUTPUT_1].trigger = SENSOR_1;
+    app_cfg_local.data.output_settings[OUTPUT_1].compressor_delay = S2ST(3* 60);
+    app_cfg_local.data.output_settings[OUTPUT_1].setpoint.unit = UNIT_TIME_MIN;
+    app_cfg_local.data.output_settings[OUTPUT_1].setpoint.value = 3;
 
-    app_cfg_local.output_settings[OUTPUT_2].function = OUTPUT_FUNC_HEATING;
-    app_cfg_local.output_settings[OUTPUT_2].trigger = SENSOR_1;
-    app_cfg_local.output_settings[OUTPUT_2].compressor_delay = S2ST(3 * 60);
-    app_cfg_local.output_settings[OUTPUT_2].setpoint.unit = UNIT_TIME_MIN;
-    app_cfg_local.output_settings[OUTPUT_2].setpoint.value = 3;
+    app_cfg_local.data.output_settings[OUTPUT_2].function = OUTPUT_FUNC_HEATING;
+    app_cfg_local.data.output_settings[OUTPUT_2].trigger = SENSOR_1;
+    app_cfg_local.data.output_settings[OUTPUT_2].compressor_delay = S2ST(3 * 60);
+    app_cfg_local.data.output_settings[OUTPUT_2].setpoint.unit = UNIT_TIME_MIN;
+    app_cfg_local.data.output_settings[OUTPUT_2].setpoint.value = 3;
 
-
-    app_cfg_local.is_valid = TRUE;
+    app_cfg_local.crc = crc32_block(0, &app_cfg_local.data, sizeof(app_cfg_data_t));
   }
 
   chMtxInit(&app_cfg_mtx);
@@ -75,6 +80,9 @@ app_cfg_idle()
 {
   if ((chTimeNow() - last_idle) > S2ST(2)) {
     chMtxLock(&app_cfg_mtx);
+
+    app_cfg_local.crc = crc32_block(0, &app_cfg_local.data, sizeof(app_cfg_data_t));
+
     if (memcmp(&app_cfg_local, &app_cfg_stored, sizeof(app_cfg_local)) != 0) {
       flashSectorErase(1);
       flashWrite((flashaddr_t)&app_cfg_stored, (char*)&app_cfg_local, sizeof(app_cfg_local));
@@ -87,7 +95,7 @@ app_cfg_idle()
 unit_t
 app_cfg_get_temp_unit(void)
 {
-  return app_cfg_local.temp_unit;
+  return app_cfg_local.data.temp_unit;
 }
 
 quantity_t
@@ -126,33 +134,33 @@ app_cfg_set_temp_unit(unit_t temp_unit)
       temp_unit != UNIT_TEMP_DEG_F)
     return;
 
-  if (temp_unit == app_cfg_local.temp_unit)
+  if (temp_unit == app_cfg_local.data.temp_unit)
     return;
 
   chMtxLock(&app_cfg_mtx);
 
   for (i = 0; i < NUM_SENSORS; ++i) {
-    sensor_settings_t* s = &app_cfg_local.sensor_settings[i];
+    sensor_settings_t* s = &app_cfg_local.data.sensor_settings[i];
     s->setpoint = quantity_convert(s->setpoint, temp_unit);
   }
 
-  app_cfg_local.temp_unit = temp_unit;
+  app_cfg_local.data.temp_unit = temp_unit;
   chMtxUnlock();
 
-  msg_broadcast(MSG_TEMP_UNIT, &app_cfg_local.temp_unit);
+  msg_broadcast(MSG_TEMP_UNIT, &app_cfg_local.data.temp_unit);
 }
 
 const matrix_t*
 app_cfg_get_touch_calib(void)
 {
-  return &app_cfg_local.touch_calib;
+  return &app_cfg_local.data.touch_calib;
 }
 
 void
 app_cfg_set_touch_calib(matrix_t* touch_calib)
 {
   chMtxLock(&app_cfg_mtx);
-  app_cfg_local.touch_calib = *touch_calib;
+  app_cfg_local.data.touch_calib = *touch_calib;
   chMtxUnlock();
 }
 
@@ -162,7 +170,7 @@ app_cfg_get_sensor_settings(sensor_id_t sensor)
   if (sensor >= NUM_SENSORS)
     return NULL;
 
-  return &app_cfg_local.sensor_settings[sensor];
+  return &app_cfg_local.data.sensor_settings[sensor];
 }
 
 void
@@ -172,7 +180,7 @@ app_cfg_set_sensor_settings(sensor_id_t sensor, sensor_settings_t* settings)
     return;
 
   chMtxLock(&app_cfg_mtx);
-  app_cfg_local.sensor_settings[sensor] = *settings;
+  app_cfg_local.data.sensor_settings[sensor] = *settings;
   chMtxUnlock();
 
   sensor_settings_msg_t msg = {
@@ -188,7 +196,7 @@ app_cfg_get_output_settings(output_id_t output)
   if (output >= NUM_OUTPUTS)
     return NULL;
 
-  return &app_cfg_local.output_settings[output];
+  return &app_cfg_local.data.output_settings[output];
 }
 
 void
@@ -198,7 +206,7 @@ app_cfg_set_output_settings(output_id_t output, output_settings_t* settings)
     return;
 
   chMtxLock(&app_cfg_mtx);
-  app_cfg_local.output_settings[output] = *settings;
+  app_cfg_local.data.output_settings[output] = *settings;
   chMtxUnlock();
 
   output_settings_msg_t msg = {
