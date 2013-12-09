afb692f479347535e1bf59f02594577f441f80ee
Nick Hebner <hebnern@gmail.com>
2013-12-08 23:12:10 -0800

    web api debug cleanup.

--- src/app_mt/web_api.c
+++ src/app_mt/web_api.c
@@ -62,22 +62,10 @@ web_api_thread(void* arg)
         websocket_error,
         api); // callback data
 
-  printf("ws connect\r\n");
-  chThdSleepSeconds(1);
-
   snError err = snWebsocket_connect(api->ws, "brewbit.herokuapp.com", NULL, NULL, 80);
-//  snError err = snWebsocket_connect(ws, "76.88.84.25", NULL, NULL, 10500);
-//  snError err = snWebsocket_connect(ws, "192.168.1.146", "echo", NULL, 12345);
-
-  printf("ws connect done\r\n");
-  chThdSleepSeconds(1);
 
   if (err != SN_NO_ERROR) {
     printf("websocket connect failed %d\r\n", err);
-  }
-  else {
-    printf("connect OK, handshaking...\r\n");
-    chThdSleepSeconds(1);
   }
 
   while (snWebsocket_getState(api->ws) != SN_STATE_OPEN) {
@@ -85,19 +73,15 @@ web_api_thread(void* arg)
     chThdSleepSeconds(1);
   }
 
-  printf("websocket is open\r\n");
-
   ApiMessage* msg = calloc(1, sizeof(ApiMessage));
   msg->type = ApiMessage_Type_ACTIVATION_TOKEN_REQUEST;
   msg->has_activationTokenRequest = true;
   strcpy(msg->activationTokenRequest.device_id, "asdfasdf");
 
-  printf("sending activation token request...\r\n");
   send_api_msg(api->ws, msg);
   free(msg);
 
   while (snWebsocket_getState(api->ws) != SN_STATE_CLOSED) {
-    printf("polling...\r\n");
     snWebsocket_poll(api->ws);
     chThdSleepSeconds(1);
   }
@@ -113,17 +97,10 @@ web_api_thread(void* arg)
 static void
 send_api_msg(snWebsocket* ws, ApiMessage* msg)
 {
-  int i;
   uint8_t* buffer = malloc(ApiMessage_size);
 
   pb_ostream_t stream = pb_ostream_from_buffer(buffer, ApiMessage_size);
   bool encoded_ok = pb_encode(&stream, ApiMessage_fields, msg);
-
-  printf("encoded %d %d\r\n", stream.bytes_written, encoded_ok);
-  for (i = 0; i < stream.bytes_written; ++i) {
-    printf("%d ", buffer[i]);
-  }
-  printf("\r\n");
 
   if (encoded_ok)
     snWebsocket_sendBinaryData(ws, stream.bytes_written, (char*)buffer);
@@ -171,27 +148,27 @@ dispatch_api_msg(web_api_t* api, ApiMessage* msg)
 
   switch (msg->type) {
   case ApiMessage_Type_ACTIVATION_TOKEN_REQUEST:
-    printf("rx activation token request\n");
+    printf("rx activation token request\r\n");
     break;
 
   case ApiMessage_Type_ACTIVATION_TOKEN_RESPONSE:
-    printf("rx activation token response\n");
+    printf("rx activation token response\r\n");
     break;
 
   case ApiMessage_Type_ACTIVATION_NOTIFICATION:
-    printf("rx activation notification\n");
+    printf("rx activation notification\r\n");
     break;
 
   case ApiMessage_Type_AUTH_REQUEST:
-    printf("rx auth request\n");
+    printf("rx auth request\r\n");
     break;
 
   case ApiMessage_Type_AUTH_RESPONSE:
-    printf("rx auth response\n");
+    printf("rx auth response\r\n");
     break;
 
   case ApiMessage_Type_DEVICE_REPORT:
-    printf("rx device report\n");
+    printf("rx device report\r\n");
     break;
 
   default:
