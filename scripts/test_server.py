ef101dbd2b9cc4f98dd7d4a05cebcc3d069dff03
Nick Hebner <hebnern@gmail.com>
2013-12-21 12:21:37 -0800

    Send a real update image from the test server.

--- scripts/test_server.py
+++ scripts/test_server.py
@@ -4,30 +4,38 @@ from autobahn.websocket import WebSocketServerFactory, \
                                listenWS
 import bbmt_pb2
 
-class EchoServerProtocol(WebSocketServerProtocol):
-   def onMessage(self, msg, binary):
-       request = bbmt_pb2.ApiMessage()
-       request.ParseFromString(msg)
-       print "Request:\n",request
+class BrewBitServerProtocol(WebSocketServerProtocol):
+    def onMessage(self, msg, binary):
+        request = bbmt_pb2.ApiMessage()
+        request.ParseFromString(msg)
+        print "Request:\n",request
        
-       response = None
-       if request.type == bbmt_pb2.ApiMessage.AUTH_REQUEST:
-           response = bbmt_pb2.ApiMessage()
-           response.type = bbmt_pb2.ApiMessage.AUTH_RESPONSE
-           response.authResponse.authenticated = True
-       elif request.type == bbmt_pb2.ApiMessage.UPDATE_REQUEST:
-           response = bbmt_pb2.ApiMessage()
-           response.type = bbmt_pb2.ApiMessage.UPDATE_CHUNK
-           response.updateChunk.offset = 0
-           response.updateChunk.data = b"123456"
-           
-       if response:
-           print "Response:\n",response
-           self.sendMessage(response.SerializeToString(), True)
+        response = None
+        if request.type == bbmt_pb2.ApiMessage.AUTH_REQUEST:
+            response = bbmt_pb2.ApiMessage()
+            response.type = bbmt_pb2.ApiMessage.AUTH_RESPONSE
+            response.authResponse.authenticated = True
+            self.send_response(response)
+        elif request.type == bbmt_pb2.ApiMessage.UPDATE_REQUEST:
+            with open('build/app_mt/app_mt_update.bin', 'rb') as f:
+                while True:
+                    response = bbmt_pb2.ApiMessage()
+                    response.type = bbmt_pb2.ApiMessage.UPDATE_CHUNK
+                    response.updateChunk.offset = f.tell()
+                    response.updateChunk.data = f.read(1024)
+                    self.send_response(response)
+                    
+                    if len(response.updateChunk.data) < 1024:
+                        break
+                   
+    def send_response(self, response):
+        if response:
+            print "Response:\n",response
+            self.sendMessage(response.SerializeToString(), True)
  
 if __name__ == '__main__':
     factory = WebSocketServerFactory("ws://localhost:9000")
-    factory.protocol = EchoServerProtocol
+    factory.protocol = BrewBitServerProtocol
     listenWS(factory)
     reactor.run()
    
