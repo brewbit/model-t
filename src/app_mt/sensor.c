ba18bcb66ab8e0b9a9be7f2dc4f3ed7128c13cd2
brianstarnes <bestarnes@gmail.com>
2014-02-17 23:05:42 -0800

    Add support for thermocouple adapter board.

--- src/app_mt/sensor.c
+++ src/app_mt/sensor.c
@@ -27,6 +27,7 @@ static void send_sensor_msg(sensor_port_t* tp, quantity_t* sample);
 static void send_timeout_msg(sensor_port_t* tp);
 
 static bool read_ds18b20(sensor_port_t* tp, quantity_t* sample);
+static bool read_max31850(sensor_port_t* tp, quantity_t* sample);
 static bool read_ds2762(sensor_port_t* tp, quantity_t* sample);
 static bool read_bb(sensor_port_t* tp, quantity_t* sample);
 
@@ -114,6 +115,9 @@ sensor_get_sample(sensor_port_t* tp, quantity_t* sample)
   }
 
   switch (addr[0]) {
+  case 0x3B:
+    return read_max31850(tp, sample);
+
   case 0x28:
     return read_ds18b20(tp, sample);
 
@@ -132,6 +136,54 @@ sensor_get_sample(sensor_port_t* tp, quantity_t* sample)
 
 static bool
 read_ds18b20(sensor_port_t* tp, quantity_t* sample)
+{
+  // issue a T convert command
+  if (!onewire_reset(tp->bus))
+    return false;
+  if (!onewire_send_byte(tp->bus, SKIP_ROM))
+    return false;
+  if (!onewire_send_byte(tp->bus, 0x44))
+    return false;
+
+  // wait for device to signal conversion complete
+  chThdSleepMilliseconds(100);
+  while (1) {
+    uint8_t bit;
+    if (!onewire_recv_bit(tp->bus, &bit))
+      return false;
+
+    if (bit)
+      break;
+    else
+      chThdSleepMilliseconds(10);
+  }
+
+  // read the scratchpad register
+  if (!onewire_reset(tp->bus)) {
+    return false;
+  }
+  if (!onewire_send_byte(tp->bus, SKIP_ROM))
+    return false;
+  if (!onewire_send_byte(tp->bus, 0xBE))
+    return false;
+  uint8_t t1, t2;
+  if (!onewire_recv_byte(tp->bus, &t1))
+    return false;
+  if (!onewire_recv_byte(tp->bus, &t2))
+    return false;
+
+  uint16_t t = (t2 << 8) + t1;
+  sample->unit = app_cfg_get_temp_unit();
+  sample->value = (t / 16.0f);
+  if (sample->unit == UNIT_TEMP_DEG_F) {
+    sample->value = (sample->value * 1.8f) + 32;
+  }
+
+  return true;
+}
+
+static bool
+read_max31850(sensor_port_t* tp, quantity_t* sample)
 {
   // issue a T convert command
   if (!onewire_reset(tp->bus))
