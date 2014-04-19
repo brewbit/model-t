2ec1c33f992cb648d9784c4023876f73dcdc2270
Nick Hebner <hebnern@gmail.com>
2014-04-19 16:30:38 -0700

    Receive output hysteresis from server.

--- src/app_mt/web_api.c
+++ src/app_mt/web_api.c
@@ -818,10 +818,13 @@ dispatch_controller_settings_from_server(ControllerSettings* settings)
     os->cycle_delay.value = osm->cycle_delay;
     os->cycle_delay.unit = UNIT_TIME_MIN;
     os->function = osm->function;
+    os->hysteresis.value = osm->hysteresis;
+    os->hysteresis.value = UNIT_TEMP_DEG_F;
 
     printf("    output %d\r\n", i);
     printf("      delay %f\r\n", os->cycle_delay.value);
     printf("      function %d\r\n", os->function);
+    printf("      function %f\r\n", os->hysteresis);
   }
 
   printf("  got sensor settings\r\n");
